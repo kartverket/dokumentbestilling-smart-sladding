a4889b5b3122ed7c8711b4840af226cbe8e25996
Jens Oskar Ramm-Pettersen <jensoskar@bekk-mac-2709.shared>
2024-07-01 15:57:16 +0200

    Parprogging av evaluation og bugfix i modellen

---
+++ evaluation_main.py
@@ -0,0 +1,24 @@
+from evaluation_dataset_utils import get_images_and_bb_from_index, visualize_bounding_boxes
+from  pdf_machinewritten_main import main
+import pandas as pd
+
+
+organized_labels_path = pd.read_csv("valideringssett/organized_data.csv")
+
+#for index, doc in range(len(organized_labels_path)):
+
+images, bbs = get_images_and_bb_from_index(organized_labels_path, 1)
+
+visualize_bounding_boxes(organized_labels_path, 1)
+
+for image, true_boxes in zip(images, bbs):
+    #Model prediction boxes
+    predicted_boxes = main(image)
+    print(true_boxes)
+    print(predicted_boxes)
+
+
+    """ matched_boxes, unmatched_predictions, metrics = emu.match_bboxes(true_boxes, predicted_boxes)
+    print(metrics) """
+
+    
