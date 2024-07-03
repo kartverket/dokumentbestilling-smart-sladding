7e3a3ed4df76255c009f25695498b01e6697eba2
oskaramm <99546385+oskaramm@users.noreply.github.com>
2024-07-03 10:37:19 +0200

    Added functionality for test and validation (#9)
    Co-authored-by: Jens Oskar Ramm-Pettersen <jensoskar@bekk-mac-2709.shared>

---
+++ model_main.py
@@ -0,0 +1,21 @@
+import model_utils
+import numpy as np
+
+def main(pdf_path):
+
+    images, dimensions = model_utils.convert_pdf_to_images(pdf_path)
+
+    predicted_boxes = []
+
+    for i, image in enumerate(images):
+        text, bounding_boxes = model_utils.extract_text_and_bb_from_image(image)
+        tagged_matches = model_utils.find_matches(text)
+        bbs = model_utils.get_boxes_to_blur(tagged_matches, bounding_boxes)
+        predicted_boxes.append(bbs)
+
+    return images, predicted_boxes
+
+
+if __name__ == '__main__':
+    images, predicted_boxes = main(pdf_path)
+    print(predicted_boxes)
