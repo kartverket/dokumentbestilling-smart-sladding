435251dd5c1b09c1afb7f91ffe4c98f138a7bf9b
Jens Oskar Ramm-Pettersen <jensoskar@bekk-mac-2709.shared>
2024-07-04 13:45:55 +0200

    redning

--- model_main.py
+++ model_main.py
@@ -6,16 +6,13 @@ def main(pdf_path):
     images, dimensions = model_utils.convert_pdf_to_images(pdf_path)
 
     predicted_boxes = []
+    texts = []
 
     for i, image in enumerate(images):
         text, bounding_boxes = model_utils.extract_text_and_bb_from_image(image)
         tagged_matches = model_utils.find_matches(text)
         bbs = model_utils.get_boxes_to_blur(tagged_matches, bounding_boxes)
         predicted_boxes.append(bbs)
+        texts.append(text)
 
-    return images, predicted_boxes
-
-
-if __name__ == '__main__':
-    images, predicted_boxes = main(pdf_path)
-    print(predicted_boxes)
+    return images, predicted_boxes, texts
