9b05ee0ca68df6a175a970216199b6b0bddf07da
oskaramm <99546385+oskaramm@users.noreply.github.com>
2024-07-04 12:53:15 +0200

    Oppryddning (#12)
    * Added functionality for test and validation
    
    * opprydning del 1
    
    * opprydning
    
    ---------
    
    Co-authored-by: Jens Oskar Ramm-Pettersen <jensoskar@bekk-mac-2709.shared>

--- evaluation_main.py
+++ evaluation_main.py
@@ -1,47 +1,24 @@
 import evaluation_utils
 import model_main
 import pandas as pd
-import matplotlib.pyplot as plt
-from PIL import ImageDraw
-import matplotlib.patches as patches
-import numpy as np
-import os
-
-def evaluate_model(folder_path):
-
-    organized_labels_path = pd.read_csv("valideringssett/organized_data.csv")
-
-    total_results = []
-    total_tp, total_fp, total_fn = 0,0,0
-    #Loop through all docu
-    for dokument in os.listdir(folder_path):
-        pdf_path = folder_path + dokument
-        #remove .pdf from the name
-        doc_id = dokument[:-4]
-        images_true, true_boxes = evaluation_utils.get_images_and_bb_from_docid(organized_labels_path, doc_id)
-        images_pred, predicted_boxes = model_main.main(pdf_path)
-        
-        metrics_list = []
-        for i,j in zip(true_boxes, predicted_boxes):
-            matched_boxes, unmatched_preds, metrics = evaluation_utils.match_bboxes(i, j)
-            metrics_list.append(metrics)
-
-        results = evaluation_utils.metrics_perdocument(metrics_list)
-        total_tp += results['TP']
-        total_fp += results['FP']
-        total_fn += results['FN']
-        
-        total_results.append(results)
-
-    print(f"Total TP: {total_tp}, Total FP: {total_fp}, Total FN: {total_fn}")
-
-    return total_results, total_tp, total_fp, total_fn
 
 
-folder_path = 'valideringssett/dokumenter/'
+organized_labels_path = pd.read_csv("valideringssett/organized_data.csv")
 
-doc_id = "2023_73325_200"
+#for index, doc in range(len(organized_labels_path)):
 
-evaluation_utils.test_and_visualize_doc(doc_id)
+images, bbs = get_images_and_bb_from_index(organized_labels_path, 1)
+
+visualize_bounding_boxes(organized_labels_path, 1)
+
+for image, true_boxes in zip(images, bbs):
+    #Model prediction boxes
+    predicted_boxes = main(image)
+    print(true_boxes)
+    print(predicted_boxes)
+
+
+    """ matched_boxes, unmatched_predictions, metrics = emu.match_bboxes(true_boxes, predicted_boxes)
+    print(metrics) """
 
     
