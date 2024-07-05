c2fcabca3281220913ddcf6f008d3461176fbeca
oskaramm <99546385+oskaramm@users.noreply.github.com>
2024-07-05 13:11:17 +0200

    added api shell (#16)
    Co-authored-by: Jens Oskar Ramm-Pettersen <jensoskar@bekk-mac-2709.shared>

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, jsonify
+import requests
+
+app = Flask(__name__)
+
+
+@app.route('/model/', methods=['POST'])
+def get_bounding_boxes():
+
+    # Hent ute dokumentinfo(rmasjon fra request body, år, id, embete
+    response = requests.get(url)
+    # response = {aar: 1980, id: 14847, embete: 101}
+    
+    #get from api
+
+    # Laste ned dokuemntet fra BestillingsAPI via url som vi lager
+
+    # Kjøre modellen på dokumentet og få tilbake bounding boxes
+
+
+    return "Bbbs" # Returnere bounding boxes i json format
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=5070)
